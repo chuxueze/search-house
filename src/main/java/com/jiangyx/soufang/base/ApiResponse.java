3d75ae2d9625f54c5961b29bae007484e740b65d
jiangyx3915 <jyx15221613915@gmail.com>
2018-01-03 22:33:54 +0800

    编写 api通用接口

---
+++ src/main/java/com/jiangyx/soufang/base/ApiResponse.java
@@ -0,0 +1,104 @@
+package com.jiangyx.soufang.base;
+
+/**
+ * API格式封装
+ */
+public class ApiResponse {
+
+    private int code;
+
+    private String message;
+
+    private Object data;
+
+    private boolean more;
+
+    public ApiResponse() {
+        this.code = Status.SUCCESS.getCode();
+        this.message = Status.SUCCESS.getStandardMessage();
+    }
+
+    public ApiResponse(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.more = more;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public boolean isMore() {
+        return more;
+    }
+
+    public void setMore(boolean more) {
+        this.more = more;
+    }
+
+    public static ApiResponse ofMessage(int code, String message) {
+        return new ApiResponse(code, message, null);
+    }
+
+    public static ApiResponse ofSuccess(Object data) {
+        return new ApiResponse(Status.SUCCESS.getCode(), Status.SUCCESS.getStandardMessage(), data);
+    }
+
+    public static ApiResponse ofStatus(Status status) {
+        return new ApiResponse(status.getCode(), status.getStandardMessage(), null);
+    }
+
+    public enum Status {
+        SUCCESS(200, "请求成功"),
+        BAD_REQUEST(400, "错误请求"),
+        INTERNAL_SERVER_ERROR(500, "未知服务器错误"),
+        NOT_VALID_PARAM(40005, "错误参数"),
+        NOT_SUPPORTED_OPERATION(40006, "非法操作"),
+        NOT_LOGIN(50000, "未登录");
+
+        private int code;
+        private String standardMessage;
+
+        Status(int code, String standardMessage) {
+            this.code = code;
+            this.standardMessage = standardMessage;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getStandardMessage() {
+            return standardMessage;
+        }
+
+        public void setStandardMessage(String standardMessage) {
+            this.standardMessage = standardMessage;
+        }
+    }
+}
