50d1ee9299242075676240abcc11fb803104de60
jiangyx3915 <jyx15221613915@gmail.com>
2018-01-06 09:36:26 +0800

    实现文件上传的配置，将图片上传到本地

--- src/main/java/com/jiangyx/soufang/controller/admin/AdminController.java
+++ src/main/java/com/jiangyx/soufang/controller/admin/AdminController.java
@@ -1,8 +1,13 @@
 package com.jiangyx.soufang.controller.admin;
 
+import com.jiangyx.soufang.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 后台管理公职器
@@ -29,5 +34,22 @@ public class AdminController {
     @GetMapping("/add/house")
     public String addHousePage() {
         return "admin/house-add";
+    }
+
+    @PostMapping(value = "/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file")MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        String filename = file.getOriginalFilename();
+        File target = new File("/home/jiangyx/IdeaProjects/sou-fang/tmp/" + filename);
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+        return ApiResponse.ofSuccess(null);
     }
 }
